3a3d148767e1be99a3c060c98b1613ded64aa1a9
pasDamola <oyindadavid18@gmail.com>
2022-01-09 23:13:34 +0100

    wrap

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,18 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import { getStoryIds } from '../services/hnApi';
-import { Story } from '../components/Story'
+import { Story } from '../components/Story';
+import { GlobalStyle, StoriesContainerWrapper } from '../styles/StoriesContainerStyles';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    getStoryIds().then(data => setStoryIds(data))
+    getStoryIds().then(data => {
+        setStoryIds(data)
+        setLoading(false)
+    })
   }, []);
 
   return (
     <>
-      <h1>Hacker News Stories</h1>
-      { storyIds.map(storyId => <Story key={storyId} storyId={storyId} />) }
+    <GlobalStyle />
+    <StoriesContainerWrapper>
+    <h1>Hacker News Stories</h1>
+      { isLoading ? 'News Loading...' : storyIds.map(storyId => <Story key={storyId} storyId={storyId} />) }
+    </StoriesContainerWrapper>
     </>  
   )
 }
